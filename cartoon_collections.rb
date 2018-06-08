5d01a52de9b40065692a2f37e7749988ebdac05a
Will Lee <will.i.lee@outlook.com>
2018-06-08 19:29:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,6 +9,7 @@ def summon_captain_planet(array)# code an argument here
   # Your code here
   array.collect do |call|
     call.capitalize + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
