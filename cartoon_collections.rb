dce702e5687945828dd7a1521cd37c0a536ebca8
Will Lee <will.i.lee@outlook.com>
2018-06-08 19:43:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.collect do |cheese|
+  cheese_types.each do |cheese|
     if array.include?(cheese)
       return cheese
     end
