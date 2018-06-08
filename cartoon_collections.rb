1416a54188515429572b8569ce26de262568fd74
Will Lee <will.i.lee@outlook.com>
2018-06-08 19:25:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.each_with_index do |i|
-    puts "#{i}. #{array[i].to_s}"
+  array.each_with_index do |item, index|
+    puts "#{index + 1}. #{array[item]}"
   end
 end
 
